9ef23568887548e02aa4dff4571d44cbb3a35db8
toshi-toma <toshitoma1217@gmail.com>
2019-07-24 12:57:31 +0900

    dom example

---
+++ dom/src/lib.rs
@@ -0,0 +1,16 @@
+use wasm_bindgen::prelude::*;
+
+// Result: ref->https://doc.rust-lang.org/std/result/
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+  let window = web_sys::window().expect("no global 'window' exists");
+  let document = window.document().expect("should have a document on window");
+  let body = document.body().expect("document should have a body");
+
+  let val = document.create_element("p")?;
+  val.set_inner_html("Hello from Rust!");
+
+  body.append_child(&val)?;
+
+  Ok(()) // return value
+}
